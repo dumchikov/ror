bbe0140e4472ed362c5efd9f872a137fde49ec88
Max <max@max-desktop.(none)>
2013-03-27 01:44:33 +0200

    base atchitecture

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site::Application.config.secret_token = '70e7f761c1af17db1528420ef34a475d208ad8c7c64d13d2cf61ee9d6552ac301e9b81a5b559953c341b38f938e0b1dae80f23c7c881494a2f8e39727478d023'
